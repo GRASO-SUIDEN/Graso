b5e5e4d50f08ca2fd15b38b2d9884783069b3e17
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-09-14 09:36:23 -1100

    Add files via upload

--- src/components/Feautures.jsx
+++ src/components/Feautures.jsx
@@ -12,6 +12,20 @@ import image5 from "../assets/Sponge 3.png";
  const Feautures = () => {
   return (
     <div className="features">
+      <div className="menuicon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+</svg>
+      </div>
+      <div className="side-nav">
+        <ul>
+          <li><div className="diamond"></div>Explore</li>
+          <li><div className="diamond"></div>Portfolio</li>
+          <li><div className="diamond"></div>Whitepaper</li>
+          <li><div className="diamond"></div>About us</li>
+           <li className="start-btn">Get started</li>
+        </ul>
+      </div>
       <div className="hero">
         <div className="text">
             <h1>Making Real Estate More Accessible to All</h1>
